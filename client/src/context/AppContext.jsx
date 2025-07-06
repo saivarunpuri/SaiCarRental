bcf1c8ec88f0339ec95149c481fd80691168238b
sai <saivarun.puri@gmail.com>
2025-07-06 14:20:06 +0530

    Midfued app context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 axios.defaults.baseURL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000";
+  import.meta.env.VITE_BASE_URL || "http://localhost:5000";
 
 const AppContext = createContext();
 
